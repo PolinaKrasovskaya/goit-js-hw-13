84f82c27c05ab3c77bf5a7e1005c79cfc6d5baec
PolinaKrasovskaya <polina.vorotnik@gmail.com>
2021-08-04 08:41:25 +0300

    finally

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '22757150-c2d7916cb8ffee93e4314d78c'
 
-async function fetchImages(name) {
+async function fetchImages(name, page) {
   const response = await axios({
     url: `${BASE_URL}`,
     params: {
@@ -11,6 +11,7 @@ async function fetchImages(name) {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page: `${page}`,
       per_page: 20,
     },
   });
